705094bc8dad71a15742dcf6dd355a438847e08e
mHaines331 <mhaines9219@gmail.com>
2023-12-07 08:13:02 -0800

    add seed data

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+cats = [
+    {
+        "name": "Goldie",
+        "species": "American Goldfinch",
+        "description": "Bright yellow plumage, state bird of New Jersey",
+        "age": 7,
+    },
+    {
+        "name": "Rosie",
+        "species": "House Finch",
+        "description": "Brown plumage, white and brown breast",
+        "age": 2,
+    },
+    {
+        "name": "Rocky",
+        "species": "Brambling",
+        "description": "Orange-breasted, black-headed finch",
+        "age": 9,
+    },
+]
+
 # Create your views here.
