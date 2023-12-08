2833ef24f6f063da06a79d06ccd62146f2fcdc20
mHaines331 <mhaines9219@gmail.com>
2023-12-08 12:17:04 -0800

    part 4

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Finch
+from .models import Finch, Feeding
 
 admin.site.register(Finch)
 # Register your models here.
+admin.site.register(Feeding)
