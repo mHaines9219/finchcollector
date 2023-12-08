ad062a3088ab5cb89f74282af721ae75993118da
mHaines331 <mhaines9219@gmail.com>
2023-12-08 14:41:50 -0800

    part 4 and 5

--- main_app/models.py
+++ main_app/models.py
@@ -11,11 +11,23 @@ MEALS = (
 )
 
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("toys_detail", kwargs={"pk": self.id})
+
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     # changing this instance method does not impact database. no migration needed at this time
     def __str__(self):
